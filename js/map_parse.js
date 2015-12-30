672f7b55eb6c942fe853ab9850cdd6ef88f13c09
jotux <joepbrown@gmail.com>
2015-12-29 22:42:17 -0800

    progress?

--- js/map_parse.js
+++ js/map_parse.js
@@ -1,6 +1,26 @@
 
+var debug = true;
+
+function db(msg)
+{
+    if (debug)
+    {
+        console.log(msg)
+    }
+}
+
 function ParseMapFile(map_data)
 {
-    // Take a map file and convert it to json for display
+    // find all the top-level sections
+    var lines = map_data.split('\n');
+    //var sections = map_data.match(/\*\(\.(.*)\)/);
+    for (var l = 0;l < lines.length;l++)
+    {
+        var section = lines[l].match(/\*\(\.(.*?)\)/);
+        if(section)
+        {
+            db(section[1]);
+        }
+    }
     return map_data;
 }
